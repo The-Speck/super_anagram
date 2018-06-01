ff844c52d9132a58cdcc37b9d045e46c66a4417c
Alex <A.plyushchay@gmail.com>
2018-05-31 22:59:23 -0400

    Fix wording

--- anagram.rb
+++ anagram.rb
@@ -1,4 +1,4 @@
-def fourth_anagram?(first, second)
+def good_anagram?(first, second)
   hash1 = Hash.new(0)
 
   first.each_char {|let| hash1[let] += 1}
@@ -9,25 +9,29 @@ end
 
 t = Time.now
 10_000_000.times{
-  fourth_anagram?("elvis", "lives")
+  good_anagram?("elvis", "lives")
 }
 p Time.now - t
 # ====> 38.4 @ 10_000_000
 
-def best_anagram?(first, second)
+
+
+def better_anagram?(first, second)
   f = first.chars
   s = second.chars
-  f == (f | s)
+  f.sort == s.sort
 end
 
 t = Time.now
 10_000_000.times{
-  best_anagram?("elvis", "lives")
+  better_anagram?("elvis", "lives")
 }
 p Time.now - t
 # ====> 23.0 @ 10_000_000
 
-def revised_anagram?(first, second)
+
+
+def best_anagram?(first, second)
   f = first.unpack('C*')
   s = second.unpack('C*')
   f == (f | s)
@@ -35,7 +39,7 @@ end
 
 t = Time.now
 10_000_000.times{
-  revised_anagram?("elvis", "lives")
+  best_anagram?("elvis", "lives")
 }
 p Time.now - t
 # ===> 14.6 @ 10_000_000
